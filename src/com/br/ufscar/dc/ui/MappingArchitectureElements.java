e92efa354a53b6d3d245bdae9ebaa5aec0b1ebc4
Fernando Chagas <fnd.chagas@gmail.com>
2014-10-21 15:13:19 -0200

    Function Map

--- src/com/br/ufscar/dc/ui/MappingArchitectureElements.java
+++ src/com/br/ufscar/dc/ui/MappingArchitectureElements.java
@@ -38,7 +38,20 @@ public class MappingArchitectureElements {
 	private Segment TOBE;
 	
 	private Segment ASIS;
+	
+	private List list;
+	
+	private Tree tree;
+	
+	private List listOfMaps;
+	
+	private ArrayList<Package> allPackagesTOBE;
+	
+	private ArrayList<Package> allPackagesASIS;
 
+	private ArrayList<MapItem> allItensToMap = new ArrayList<MapItem>();
+	
+	private EList<AbstractStructureElement> structureElementsTOBE;
 	/**
 	 * Open the window.
 	 */
@@ -56,20 +69,21 @@ public class MappingArchitectureElements {
 
 	/**
 	 * Create contents of the window.
+	 * @wbp.parser.entryPoint
 	 */
 	protected void createContents() {
 		this.TOBE = this.readingKDMFile.load("file:C:/Users/Fernando/Documents/workspace/ProjetoMestradoFernandoChagas2/src/planned.xmi");
 		this.ASIS = this.readingKDMFile.load("file:C:/Users/Fernando/Documents/workspace/ProjetoMestradoFernandoChagas2/src/newKDM.xmi");
 		
 		StructureModel structureModelTOBE = readingKDMFile.getStructureModelPassingSegment(this.TOBE);
-		EList<AbstractStructureElement> structureElementsTOBE = structureModelTOBE.getStructureElement();
+		structureElementsTOBE = structureModelTOBE.getStructureElement();
 		
-		ArrayList<Package> allPackagesTOBE = readingKDMFile.getAllPackages(this.TOBE);
-		ArrayList<Package> allPackagesASIS = readingKDMFile.getAllPackages(this.ASIS);
+		this.allPackagesTOBE = readingKDMFile.getAllPackages(this.TOBE);
+		this.allPackagesASIS = readingKDMFile.getAllPackages(this.ASIS);
 		
 		
 		shlMappingArchitectureElements = new Shell();
-		shlMappingArchitectureElements.setSize(657, 456);
+		shlMappingArchitectureElements.setSize(657, 472);
 		shlMappingArchitectureElements.setText("Mapping Architecture Elements");
 		
 		Label lblArchitecturalElements = new Label(shlMappingArchitectureElements, SWT.NONE);
@@ -77,7 +91,7 @@ public class MappingArchitectureElements {
 		lblArchitecturalElements.setBounds(10, 10, 199, 21);
 		lblArchitecturalElements.setText("Architectural Elements");
 		
-		List list = new List(shlMappingArchitectureElements, SWT.BORDER);
+		list = new List(shlMappingArchitectureElements, SWT.BORDER);
 		list.setFont(SWTResourceManager.getFont("Lucida Grande", 12, SWT.NORMAL));
 		for (AbstractStructureElement element : structureElementsTOBE) {
 			list.add(element.getName());
@@ -86,17 +100,16 @@ public class MappingArchitectureElements {
 		//list.setItems(new String[] {"layer - model", "layer - controller", "layer - view"});
 		list.setBounds(10, 37, 294, 198);
 		
-		List list_1 = new List(shlMappingArchitectureElements, SWT.BORDER);
-		list_1.setFont(SWTResourceManager.getFont("Lucida Grande", 12, SWT.NORMAL));
-		list_1.setItems(new String[] {"layer model: package com.br.model", "layer controller: package com.br.controller", "layer view: package com.br.view"});
-		list_1.setBounds(10, 268, 625, 123);
+		listOfMaps = new List(shlMappingArchitectureElements, SWT.BORDER | SWT.H_SCROLL);
+		listOfMaps.setFont(SWTResourceManager.getFont("Lucida Grande", 12, SWT.NORMAL));
+		listOfMaps.setBounds(10, 268, 625, 123);
 		
 		Label lblArchitecturalElementsMapped = new Label(shlMappingArchitectureElements, SWT.NONE);
 		lblArchitecturalElementsMapped.setFont(SWTResourceManager.getFont("Lucida Grande", 14, SWT.NORMAL));
 		lblArchitecturalElementsMapped.setText("Architectural Elements Mapped\n");
 		lblArchitecturalElementsMapped.setBounds(10, 241, 275, 21);
 		
-		Tree tree = new Tree(shlMappingArchitectureElements, SWT.BORDER);
+		tree = new Tree(shlMappingArchitectureElements, SWT.CHECK | SWT.BORDER | SWT.MULTI);
 		tree.setFont(SWTResourceManager.getFont("Lucida Grande", 12, SWT.NORMAL));
 		tree.setBounds(329, 37, 306, 197);
 		
@@ -113,15 +126,99 @@ public class MappingArchitectureElements {
 			treeItem0.setImage(packageImage);
 			treeItem0.setText(pathToGet);
 			
-			EList<AbstractCodeElement> allClassesAndInterfaces = readingKDMFile.getClassesAndInterfacesByPackage(package1);
+			EList<AbstractCodeElement> allClassesAndInterfaces = readingKDMFile.getClassesAndInterfacesByPackage(package1);	
 			for (AbstractCodeElement classOrInterface : allClassesAndInterfaces) {
 				treeItem11 = new TreeItem(treeItem0, 0);
 				treeItem11.setImage(classImage);
-				treeItem11.setText(classOrInterface.getName() + ".java");
+				treeItem11.setText(classOrInterface.getName() + ".java");				
 			}
 		}
 		
+	
+		
 		Button btnMap = new Button(shlMappingArchitectureElements, SWT.NONE);
+		btnMap.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				MapItem mapItem = new MapItem();
+				//getting item selected on the list
+				int ind = list.getSelectionIndex();
+				//saving architecture element to save
+				mapItem.setStructureElement(structureElementsTOBE.get(ind));
+				
+				TreeItem [] selTree = tree.getSelection();
+				//System.out.println("IndicesSelecionados: " + structureElementsTOBE.get(ind).getName());
+				//System.out.println("Tree");
+
+				String map = "";
+				map+= mapItem.getStructureElement().getName() + " mapped to ";
+				
+				for (int i = 0; i < selTree.length; i++) {
+					
+					//caso possua pai
+					if (selTree[i].getParentItem() != null) {
+						
+						String pathToGet = "";
+						
+						
+						for (Package package1 : allPackagesASIS) {
+							pathToGet = "";
+							pathToGet = readingKDMFile.getPathOfPackage(package1, pathToGet);
+							if (pathToGet.equals(selTree[i].getParentItem().getText())) {
+								EList<AbstractCodeElement> elements = readingKDMFile.getClassesAndInterfacesByPackage(package1);
+								for (AbstractCodeElement abstractCodeElement : elements) {
+									String elementToFind = selTree[i].getText().replace(".java", "");
+									//System.out.println("Element to Find: " + elementToFind);
+									if (elementToFind.equals(abstractCodeElement.getName())) {
+										mapItem.getCodeElements().add(abstractCodeElement);
+										allItensToMap.add(mapItem);
+										//adding to list																																																
+										
+										break;
+									}
+								}
+							}
+						}
+
+					} else {
+						
+						String pathToGet = "";
+						
+						for (Package package1 : allPackagesASIS) {
+							pathToGet = "";
+							pathToGet = readingKDMFile.getPathOfPackage(package1, pathToGet);
+							if (pathToGet.equals(selTree[i].getText())) {
+								//System.err.println("encontrou o pacote");
+								mapItem.getCodeElements().add(package1);
+								allItensToMap.add(mapItem);
+							}
+						}
+						
+					}
+					
+					
+					AbstractCodeElement itemToMap = mapItem.getCodeElements().get(mapItem.getCodeElements().size()-1);
+					if (i == (selTree.length-1)) {
+						//System.out.println("ultimo");
+						if (itemToMap instanceof Package)
+							map += itemToMap.getName() + ".";
+						else 
+							map += itemToMap.getName() + ".java.";
+						
+						listOfMaps.add(map);
+					} else {
+						//System.out.println(mapItem.getCodeElements().get(mapItem.getCodeElements().size()-1).getName());
+						
+						if (itemToMap instanceof Package)
+							map += itemToMap.getName() + ", ";
+						else 
+							map += itemToMap.getName() + ".java, ";
+					}
+				}
+
+				
+			}
+		});
 		btnMap.setFont(SWTResourceManager.getFont("Lucida Grande", 14, SWT.NORMAL));
 		btnMap.setBounds(541, 234, 94, 28);
 		btnMap.setText("Map");
@@ -140,6 +237,16 @@ public class MappingArchitectureElements {
 		});
 		btnDelete.setText("Delete");
 		btnDelete.setBounds(541, 397, 94, 28);
+		
+		Button btnGenerate = new Button(shlMappingArchitectureElements, SWT.NONE);
+		btnGenerate.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+			}
+		});
+		btnGenerate.setText("Generate");
+		btnGenerate.setFont(SWTResourceManager.getFont("Lucida Grande", 14, SWT.NORMAL));
+		btnGenerate.setBounds(10, 397, 94, 28);
 
 	}
 	
